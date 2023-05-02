4476960e9aae136a538bc0d608e70160340bc464
David Nakeeran <127234789+Jamzul@users.noreply.github.com>
2023-05-02 14:27:26 +0100

    created playGame function, taking input from user and computerChoice, run through if else, to determine winner of round.

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 function getComputerChoice() {
     const num = Math.floor(Math.random() * 3) + 1;
     if (num === 1) {
-        return 'Rock';
+        return 'rock';
     }
     else if (num === 3) {
-        return 'Scissors';
+        return 'scissors';
     }
     else (num === 2) 
-        return 'Paper';
-    
-
+        return 'paper';
 }
-console.log(getComputerChoice());
+
+
+
+
+
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie';
+    } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+        return 'You Win, paper beats rock!';
+    } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+        return 'You Win, rock beats scissors!';
+    } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+        return 'You win, scissors beats paper!';
+    } else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
+        return 'You lose, paper beats rock';
+    } else if ((playerSelection === 'scissors') && (computerSelection === 'rock')) {
+        return 'You lose, rock beats scissors';
+    } else if ((playerSelection === 'paper') && (computerSelection === 'scissors')) {
+        return 'You lose, scissors beats paper';
+    }
+
+} 
+
+const computerSelection = getComputerChoice();
+const userInput = prompt('Please type either Rock, Paper or Scissors');
+const playerSelection = userInput.toLowerCase();
+console.log(computerSelection);
+console.log(playGame(playerSelection, computerSelection));
+
+
+
+
